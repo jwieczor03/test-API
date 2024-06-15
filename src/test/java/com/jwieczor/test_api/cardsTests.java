dca15d792aef96d83751a514d13ffa158624c9fc
jwieczor <jwieczor@op.pl>
2024-06-15 18:56:21 +0200

    feat: testing get all cards endpoint

---
+++ src/test/java/com/jwieczor/test_api/cardsTests.java
@@ -0,0 +1,74 @@
+package com.jwieczor.test_api;
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+
+public class cardsTests {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://api.magicthegathering.io/v1";
+    }
+
+    @Test
+    public void testGetAllCards() {
+        given()
+                .when()
+                .get("/cards")
+                .then()
+                .statusCode(200)
+                .header("Count", equalTo("100"));
+    }
+
+    @Test
+    public void wrongMethod() {
+        given()
+                .when()
+                .post("/cards")
+                .then()
+                .statusCode(405);
+    }
+
+    @Test
+    public void checIfIsNotNull() {
+        Response response =
+            given()
+                    .when()
+                    .get("/cards")
+                    .then()
+                    .extract().response();
+        response.then().body("size()", greaterThan(0));
+
+    }
+
+    @Test
+    public void checkFirstRecord() {
+
+        Response response = get("/cards");
+        String name = response.jsonPath().getString("cards[0].name");
+        assertThat(name, equalTo("Ancestor's Chosen"));
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
