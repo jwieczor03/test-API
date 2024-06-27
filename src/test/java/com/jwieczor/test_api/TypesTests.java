da39441958ba622bb6dccd893cedc963a0fb2a7a
jwieczor <jwieczor@op.pl>
2024-06-27 23:15:09 +0200

    feat: testing  get all types

--- src/test/java/com/jwieczor/test_api/TypesTests.java
+++ src/test/java/com/jwieczor/test_api/TypesTests.java
@@ -6,25 +6,28 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 
-public class BoosterTests {
+public class TypesTests {
+
 
     @BeforeAll
     public static void setup() {
         RestAssured.baseURI = "https://api.magicthegathering.io/v1";
     }
 
+
     @Test
     public void testGetAllCards() {
         Response response =
                 given()
                         .when()
-                        .get("/sets/ktk/booster")
+                        .get("/types")
                         .then()
                         .statusCode(200)
                         .extract().response();
 
-        response.then().body("set.size()", greaterThan(0));
+        response.then().body("types.size()", equalTo(27));
     }
 }
