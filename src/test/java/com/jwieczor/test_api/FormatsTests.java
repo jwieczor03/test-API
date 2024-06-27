cd64a9dca79598a0c28207632d6f22a3cf43d23f
jwieczor <jwieczor@op.pl>
2024-06-27 23:46:04 +0200

    feat: testing  get all formats

---
+++ src/test/java/com/jwieczor/test_api/FormatsTests.java
@@ -0,0 +1,33 @@
+package com.jwieczor.test_api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class FormatsTests {
+
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://api.magicthegathering.io/v1";
+    }
+
+
+
+    @Test
+    public void testGetAllCards() {
+        Response response =
+                given()
+                        .when()
+                        .get("/formats")
+                        .then()
+                        .statusCode(200)
+                        .extract().response();
+
+        response.then().body("formats.size()", equalTo(23));
+    }
+}
